71e724cffa2f8363de71b8038c3e2c3f0470c04a
Egor Gorskikh <egorgorskikh@gmail.com>
2021-07-21 14:14:12 +0300

    init: start project

---
+++ Pryaniky/PryanikyModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pryaniky
+//
+//  Created by Egor Gorskikh on 21.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
